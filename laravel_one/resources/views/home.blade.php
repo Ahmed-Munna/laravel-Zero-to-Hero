927c0fa384eca22b1d55f03d9554dbef2d96513a
Ahmed-Munna <ahmedmunna4200@gmail.com>
2023-07-27 19:34:50 +0600

    laravel

---
+++ laravel_one/resources/views/home.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h2>HTML Forms</h2>
+
+<form method="put" action="{{route('data')}}" >
+    @csrf
+  <label for="fname">First name:</label><br>
+  <input type="text" id="fname" name="fname" value="John"><br>
+  <label for="lname">Last name:</label><br>
+  <input type="text" id="lname" name="lname" value="Doe"><br><br>
+  <input type="submit" value="Submit">
+</form> 
+
+</body>
+</html>
--- laravel_one/routes/web.php
+++ laravel_one/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +15,38 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+// Route::redirect('/','/home',392);
+// Route::get('/home', function () {
+//     return view('home');
+// });
+// Route::match(['post', 'put'], '/data', function (Request $data) {
+//     return $data;
+// })->name('data');
+Route::any('/data', function (Request $request) {
+    return print_r($request);
+})->name('data');
+
+Route::view('/home', 'home', ['name' => 'Munna']);
+
+// Route::get('/user/{id}', function (string $id) {
+//     return 'user = ' . $id;
+// });
+
+// Route::get('/user/{id?}', function (Request $request, string $id = null) {
+//     return 'User = ' . $id;
+// });
+
+// Route::get('user/{name}', function (string $name) {
+//     return 'User = ' . $name;
+// })->whereAlpha('name');
+// Route::get('user/{name}', function (string $name) {
+//     return 'User = ' . $name;
+// })->whereAlphaNumeric('name');
+// Route::get('user/{name}', function (string $name) {
+//     return 'User = ' . $name;
+// })->whereIn('name', ['munna','masud']);
+
+Route::get('/user/{id}', function (string $id) {
+    return 'User = ' . $id;
 });
