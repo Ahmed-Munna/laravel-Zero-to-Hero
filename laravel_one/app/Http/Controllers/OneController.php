08a6eba6de9888dc0fa9ef815edf9e7b16ea72b5
Ahmed-Munna <ahmedmunna4200@gmail.com>
2023-07-28 06:41:03 +0600

    laravel route

---
+++ laravel_one/app/Http/Controllers/OneController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class OneController extends Controller
+{
+    public function show() {
+        return "something";
+    }
+
+}
--- laravel_one/routes/web.php
+++ laravel_one/routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
+use App\Http\Controllers\OneController;
+use App\Models\User;
+use App\Models\Post;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,7 +19,7 @@ use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('wel');
 // Route::redirect('/','/home',392);
 // Route::get('/home', function () {
 //     return view('home');
@@ -47,9 +51,34 @@ Route::view('/home', 'home', ['name' => 'Munna']);
 //     return 'User = ' . $name;
 // })->whereIn('name', ['munna','masud']);
 
-Route::get('/user/{id}', function (Request $request,string $id) {
-    if ($request->route()->named('user')) {
-        return 'User = ' . $id . $_GET['name'] . 'Yes This is named route';
-    } 
+// Route::get('/user/{id}', function (Request $request,string $id) {
+//     if ($request->route()->named('user')) {
+//         return 'User = ' . $id . $_GET['name'] . 'Yes This is named route';
+//     } 
     
-})->name('user');
+// })->name('user');
+
+// Route::middleware('web')->group(function() {
+//     Route::get('/user/some', function () {
+//         // bal chall heda
+//     });
+// });
+
+// Route::controller(OneController::class)->group(function () {
+//     Route::get('/users/some', 'show');
+//     Route::post('/user/blabla', function () {
+//         //whatever you want
+//     });
+// });
+
+// Route::get('/user/{user:name}',  [OneController::class, 'show']);
+
+// Route::get('/user/{post:name}/{user:name}', function (Post $post, User $user) {
+//     return $post;
+// });
+
+Route::get('/user/{user}', [OneController::class, 'show'])
+                        ->missing(function (Request $request) {
+
+                            return Redirect::route('data');
+                        });
