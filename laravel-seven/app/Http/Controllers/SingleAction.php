75c13ad12a2dd48b5b6598f716a7573cfbce9ec9
Ahmed-Munna <ahmedmunna4200@gmail.com>
2024-01-06 21:56:12 +0600

    laravel

--- laravel-seven/app/Http/Controllers/SingleAction.php
+++ laravel-seven/app/Http/Controllers/SingleAction.php
@@ -6,12 +6,9 @@ use Illuminate\Http\Request;
 
 class SingleAction extends Controller
 {
-    /**
-     * Handle the incoming request.
-     */
+    
     public function __invoke(Request $request):string
     {
-        //
 
         return "Hello, I'm Single Action Controller";
     }
