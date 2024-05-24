71378c0af84994c9920b4c3cb2af3e6ee5abe1e1
Ahmed-Munna <ahmedmunna4200@gmail.com>
2024-05-24 14:57:11 +0600

    laravel

--- laravel-20/app/Http/Kernel.php
+++ laravel-20/app/Http/Kernel.php
@@ -64,5 +64,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'isLogedIn' => \App\Http\Middleware\IsLogedIn::class,
+        'isAdminLogedIn' => \App\Http\Middleware\IsAdminLogedIn::class,
     ];
 }
--- laravel-20/routes/web.php
+++ laravel-20/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,8 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
+// User Routes
 Route::view('/login', 'Auth.sign-in')->name('login')->middleware('isLogedIn');
 Route::view('/register', 'Auth.sign-up')->name('register')->middleware('isLogedIn');
 Route::view('/dashboard', 'dashboard')->name('dashboard')->middleware('auth');
@@ -25,3 +27,13 @@ Route::view('/dashboard', 'dashboard')->name('dashboard')->middleware('auth');
 Route::post('/login-user', [UserController::class, 'login'])->name('login-user');
 Route::post('/register-user', [UserController::class, 'registerUser'])->name('register-user');
 Route::get('/logout', [UserController::class, 'logout'])->name('logout')->middleware('auth');
+
+// Admin Routes
+
+Route::view('/admin-login', 'Admin.sign-in')->name('admin-login');
+Route::view('/admin-register', 'Admin.sign-up')->name('admin-register');
+Route::view('/admin-dashboard', 'Admin.dashboard')->name('admin-dashboard')->middleware('auth:admin');
+
+Route::post('/login-admin', [AdminController::class, 'login'])->name('login-admin');
+Route::post('/register-admin', [AdminController::class, 'registerUser'])->name('register-admin');
+//Route::get('/logout-admin', [AdminController::class, 'logout'])->name('logout')->middleware('auth');
